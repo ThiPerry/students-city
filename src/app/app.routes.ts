7965584258b4f8a7f08595a488a7f619911b3174
thibaut <perry@et.esiea.fr>
2025-06-15 17:14:07 +0200

    Ajout Profile et navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { LoginPage } from './auth/login/login.page';
-import { RegisterPage } from './auth/register/register.page';
-import { HomePage } from './home/home.page';
-import { MapViewPage} from './pages/map-view/map-view.page';
-import { AuthGuard } from './guard/auth-guard.guard';
+import { LoginPage }      from './auth/login/login.page';
+import { RegisterPage }   from './auth/register/register.page';
+import { HomePage }       from './home/home.page';
+import { MapViewPage }    from './pages/map-view/map-view.page';
+import { ProfilePage }    from './pages/profile/profile.component';
+import { AuthGuard }      from './guard/auth-guard.guard';
 
+// Ajoutez `export` ici
 export const routes: Routes = [
-  { path: 'auth/login',component: LoginPage },
+  { path: 'auth/login',    component: LoginPage },
   { path: 'auth/register', component: RegisterPage },
-  { path: 'map',    canActivate:[AuthGuard], component: MapViewPage },
-  { path: 'home',    canActivate:[AuthGuard], component: HomePage },
-  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
-
+  { path: 'profile',       component: ProfilePage, canActivate: [AuthGuard] },
+  { path: 'map',           component: MapViewPage, canActivate: [AuthGuard] },
+  { path: 'home',          component: HomePage,    canActivate: [AuthGuard] },
+  { path: '',              redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: '**',            redirectTo: 'map' }
 ];
