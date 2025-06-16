7218af6cf5272944c5859d37d1f2cbe9459b83d3
thibaut <perry@et.esiea.fr>
2025-06-16 12:03:03 +0200

    Correction splashscreen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'StudentsCity',
+  appId: 'com.students.city',
+  appName: 'students-city',
   webDir: 'www',
   plugins: {
     SplashScreen: {
